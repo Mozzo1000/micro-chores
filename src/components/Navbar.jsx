84dfcbcca1d46f85d3ffcc3fb38c64e6417785ff
Andreas Backström <mozzo242@gmail.com>
2023-03-13 18:16:40 +0100

    Fix navbar button not visible with light theme #1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
 
     return (
         <nav className="flex items-center justify-between flex-wrap bg-transparent absolute p-8">
-            <div className="flex items-center flex-shrink-0 text-white">
+            <div className="flex items-center flex-shrink-0">
                 <button className="px-3 py-2 rounded hover:text-slate-400" onClick={() => setShowMobileNav(!showMobileNav)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
