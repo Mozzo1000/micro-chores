4063e316ba75ac2195cdee2fe3f40aaa3f8d72d5
Andreas Backström <mozzo242@gmail.com>
2023-03-13 20:32:52 +0100

    Add flowbite and react-spotify-embed dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,13 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
   ],
 }
