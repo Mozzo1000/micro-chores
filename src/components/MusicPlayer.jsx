22742795861380d5213fc7cfa487033c1c89b39a
Andreas Backström <andreas@simplymozzo.se>
2024-03-17 16:20:22 +0100

    Update all dependencies

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -10,14 +10,14 @@ function MusicPlayer() {
             <div
                 className={`p-4 pt-3 bg-stone-900 h-4/6 bottom-0 text-white fixed z-38 ease-in-out duration-300 ${showMusicPlayer ? "translate-y-0" : "translate-y-[92%]"}`}>
                 <p className="pb-4 font-bold" onClick={() => setShowMusicPlayer(!showMusicPlayer)}>Music player</p>
-                <Tabs.Group style="pills">
+                <Tabs style="pills">
                     <Tabs.Item active={true} title="Spotify">
                         <Spotify link="https://open.spotify.com/playlist/37i9dQZF1DWWQRwui0ExPn" />
                     </Tabs.Item>
                     <Tabs.Item title="Youtube">
                         <iframe width="300" height="315" src="https://www.youtube.com/embed/jfKfPfyJRdk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </Tabs.Item>
-                </Tabs.Group>
+                </Tabs>
             </div>
         </div>
     )
